c720966e02e9122f57bc7a45c41d834d8ac53b8b
Nalinihatti <nalinihatti@gmail.com>
2025-10-25 00:09:16 +0530

    variable
    variable types and declaration

---
+++ day1/variable1.js
@@ -0,0 +1,28 @@
+// Variable -> Container to store data/value
+/*
+1. String 
+2. Number 
+3. Boolean
+4.object -> null
+undefined */
+
+fullName = "Nalini";
+console.log("Name = ", fullName);
+console.log("Type Name = ", typeof fullName);
+
+fullName = 10;
+console.log("Name = ", fullName);
+console.log("Type Name = ", typeof fullName);
+
+fullName = true;
+console.log("Name = ", fullName);
+console.log("Type Name = ", typeof fullName);
+
+fullName = null;
+console.log("Name = ", fullName);
+console.log("Type Name = ", typeof fullName);
+
+// variable declaration
+let num;
+console.log("Value of num = ", num);
+console.log("Type of num = ", typeof num);
