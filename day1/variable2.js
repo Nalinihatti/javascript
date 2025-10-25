c5f10bd4d16e00909a5f89e8ddca014914bab153
Nalinihatti <nalinihatti@gmail.com>
2025-10-25 19:12:31 +0530

    variables

---
+++ day1/variable2.js
@@ -0,0 +1,17 @@
+const pi = 3.148799;
+
+console.log("Value of pi = ", pi);
+
+// Constant cannot be reassigned
+// pi = 9.81;
+// console.log("Value of pi = ", pi);
+
+// Let is used to declare/define a variable with let keyword we can reassign values
+let num;
+console.log("Value of num is = ", num);
+
+num = 18;
+console.log("Value of num is = ", num);
+// let num = 8520; variable with let cannot be redeclare
+
+
---
+++ day1/variable3.js
@@ -0,0 +1,15 @@
+// Addition of 2 numbers
+
+let n1 = 30;
+let n2 = 20;
+
+console.log("Addition of ", n1 ,",", n2 , " = ", n1 + n2);
+// Formatted strings
+console.log(`Additon of ${n1},${n2} = ${n1 + n2}`);
+
+console.log('Hi I am "Nalini Hatti"');
+console.log("Hi I am 'Nalini Hatti'");
+
+console.log(`Hi I am "Nalini Hatti"`);
+
+
