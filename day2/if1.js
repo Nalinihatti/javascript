c1ead1bafcb4085b467fe12e3582d2328a010304
Nalinihatti <nalinihatti@gmail.com>
2025-11-08 00:29:42 +0530

    relational operators

---
+++ day2/if1.js
@@ -0,0 +1,13 @@
+// conditional Statements (if else)
+// WAP to check whether the number is positive or negative
+// input -> 1, conditions = 2 (if else)
+let num = 50;
+if(num >= 0){
+    console.log(`Number = ${num} is positive number`);
+    
+}
+else{
+    console.log(`Number = ${num} is Negative number`);
+
+}
+   
---
+++ day2/relational1.js
@@ -0,0 +1,19 @@
+// Relational Operator -> True / False
+/*
+1. Greater Than: >
+2. Less Than: <
+3. Greater Than Or Equal To: >=
+4. Less Than Or Equal To: <=
+5. Equal To Equal To: ==
+6. Not Equal To: !=
+*/
+
+let n1 = 60;
+let n2 = 40;
+
+console.log(`${n1} > ${n2} = ${n1 > n2}`);
+console.log(`${n1} < ${n2} = ${n1 < n2}`);
+console.log(`${n1} >= ${n2} = ${n1 >= n2}`);
+console.log(`${n1} <= ${n2} = ${n1 <= n2}`);
+console.log(`${n1} == ${n2} = ${n1 == n2}`);
+console.log(`${n1} != ${n2} = ${n1 != n2}`);
