1f9254bbc2ff7b11f552c2e4be1bde030d3f1c2d
Nalinihatti <nalinihatti@gmail.com>
2025-11-08 18:19:31 +0530

    nested if else

---
+++ day3/if4.js
@@ -0,0 +1,23 @@
+// Nested If else
+// if(){if else}
+
+/* WAP to check whether the triangle is 
+Input = 3, conditions = 3
+1. Equilateral -> all sides are equal
+2. Isoceles -> Any 2 sides are same
+3. Scalene  
+*/
+
+let s1 = 5;
+let s2 = 5;
+let s3 = 5;
+
+if(s1 == s2 && s2 == s3){
+    console.log("Triangle is Equilateral");
+    
+}
+else if(s1 == s2 || s2 == s3) {
+    console.log("Triangle is Isoceles");
+} else {
+    console.log("Triangle is Scalene");
+}
