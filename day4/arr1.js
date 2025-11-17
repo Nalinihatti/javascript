f340eee73dc1f72b256ebd0d0918dfb4bacd491e
Nalinihatti <nalinihatti@gmail.com>
2025-11-17 22:36:09 +0530

    array

---
+++ day4/arr1.js
@@ -0,0 +1,25 @@
+// Arrays: Collections of different type data
+
+numbers = [5, 1, 7, 9, 4];
+console.log(numbers);
+console.log(typeof numbers);
+
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+numbers[1] = -43; // modify
+
+console.log(numbers);
+
+student_info = ["KAVERI HATTI", 18, "FEMALE", 75.44];
+console.log(student_info);
+console.log(typeof student_info);
+console.log(student_info[0]);
+console.log(student_info[0][4]);
+
+
+
+
+
